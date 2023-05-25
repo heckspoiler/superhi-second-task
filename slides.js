cfe6cbcf526da4fcaa03bd75a8201d1d9e4b55b8
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-25 21:24:32 +0200

    added slide animation

--- slides.js
+++ slides.js
@@ -6,3 +6,24 @@ const images = slideArea.querySelectorAll("img");
 // keep track of two things
 let currentSlide = 0;
 let z = 1;
+
+// when i click slide area, change slide on z-index
+
+slideArea.addEventListener("click", function () {
+  currentSlide = currentSlide + 1;
+
+  if (currentSlide > images.length - 1) {
+    currentSlide = 0;
+  }
+
+  z = z + 1;
+
+  // remove the animation from the style for every image
+  images.forEach((image) => {
+    image.style.animation = "";
+  });
+
+  // pick the right image
+  images[currentSlide].style.zIndex = z;
+  images[currentSlide].style.animation = "fade 0.5s";
+});
